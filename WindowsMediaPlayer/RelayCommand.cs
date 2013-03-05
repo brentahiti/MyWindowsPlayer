351528db76e0e225f002146eab597c8be9ba6090
yau_t <tom-brent.yau@epitech.eu>
2013-03-05 18:53:23 +0100

    Add RelayCommand

---
+++ WindowsMediaPlayer/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace WindowsMediaPlayer
+{
+    class RelayCommand : ICommand
+    {
+        private Action action;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action a)
+        {
+            this.action = a;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (this.action != null)
+                this.action();
+        }
+    }
+}

4038aa8b98f455ecf2ae0739f18adad791056fe5
yau_t <tom-brent.yau@epitech.eu>
2013-03-19 19:43:28 +0100

    Remove useless code from Code behind

--- WindowsMediaPlayer/MainWindow.xaml.cs
+++ WindowsMediaPlayer/MainWindow.xaml.cs
@@ -17,18 +17,11 @@ using System.Windows.Interop;
 
 namespace WindowsMediaPlayer
 {
-
-    public enum eRead {Nothing, Play, Pause};
-    public enum eStop { Nothing, Stop };
-    public enum ePath { Nothing, CorrectPath };
-
     /// <summary>
     /// View of WMP
     /// </summary>
     public partial class MainWindow : Window
     {
-        protected Modele Action;
-
         public const int WM_NCLBUTTONDOWN = 0xA1;
         public const int HT_CAPTION = 0x2;
         private const int WM_SYSCOMMAND = 0x112;
@@ -64,7 +57,6 @@ namespace WindowsMediaPlayer
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            this.Action = new Modele();
             this.hwndSource = PresentationSource.FromVisual((Visual)sender) as HwndSource;
 
             var row = this.MainLayoutGrid.ColumnDefinitions;
@@ -253,77 +245,6 @@ namespace WindowsMediaPlayer
         private void mediaElement_Drop(object sender, DragEventArgs e)
         {
             Console.WriteLine((string)((DataObject)e.Data).GetFileDropList()[0]);
-        }
-    }
-
-    /// <summary>
-    /// Modele of WMP
-    /// </summary>
-    public class Modele
-    {
-        private MediaPlayer player;
-        private string path = null;
-        private bool isReading = false;
-        private bool pause = false;
-
-        public Modele()
-        {
-            this.player = new MediaPlayer();
-            this.player.Volume = 0.0;
-        }
-
-        public ePath ModelePath()
-        {
-            OpenFileDialog windowsDial = new OpenFileDialog();
-            windowsDial.FileName = "File";
-            windowsDial.DefaultExt = ".avi";
-            windowsDial.Filter = "Music file (.mp3)|*.mp3| " + "Video File (.avi)|*.avi";
-
-            Nullable<bool> result = windowsDial.ShowDialog();
-            if (result == true)
-            {
-                this.path = windowsDial.FileName;
-                this.isReading = false;
-                this.pause = false;
-                return ePath.CorrectPath;
-            }
-            return ePath.Nothing;
-        }
-
-        public eStop ModeleStop()
-        {
-            if (this.pause == true || this.isReading == true)
-            {
-                this.isReading = false;
-                this.pause = false;
-                this.player.Stop();
-                return eStop.Stop;
-            }
-            return eStop.Nothing;
-        }
-
-        public eRead ModeleRead()
-        {
-            if (this.path != null)
-            {
-
-                if (this.isReading == true && this.pause == false)
-                {
-                    this.player.Pause();
-                    this.pause = true;
-                    this.isReading = false;
-                    return eRead.Pause;
-                }
-                if (this.isReading == false && this.pause == false)
-                {
-                    this.player.Open(new System.Uri(this.path));
-                }
-                this.player.Play();
-                this.isReading = true;
-                this.pause = false;
-                return eRead.Play;
-            }
-            return eRead.Nothing;
         }
     }
 }
